51ba0bd6fa73c8d7817fb22b8e3c2cb7785d2009
Developer <user@example.com>
2026-05-17 00:36:13 +0700

    Remove Diagrams folder

--- MVVM_Champ/obj/Debug/net9.0-windows/MVVM_Champ.AssemblyInfo.cs
+++ MVVM_Champ/obj/Debug/net9.0-windows/MVVM_Champ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVVM_Champ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675a7b9a3abb8a32180860373851518b1be7bcca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f3590eab877affb1100b7198d3707a331d8426")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVVM_Champ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVVM_Champ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
